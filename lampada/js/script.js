93621131a9504bed5116d80384561d11e7d8cbf5
Valquíria Lourenço <lourencotecnologia25@gmail.com>
2024-12-06 16:37:25 -0300

    .

--- lampada/js/script.js
+++
@@ -1,6 +0,0 @@
-function Acender(){
-    document.getElementById('imagem').src='https://www.w3schools.com/js/pic_bulbon.gif' 
-}
-function Apagar(){
-    document.getElementById('imagem').src='https://www.w3schools.com/js/pic_bulboff.gif'
-}
